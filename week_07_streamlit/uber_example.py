abc3724f29f2052d0c032adfafb45f2540fe2f60
Etienne BRUNO <etienne.bruno@epfl.ch>
2023-04-06 15:38:12 +0200

    add streamlit

---
+++ week_07_streamlit/uber_example.py
@@ -0,0 +1,46 @@
+"""An example of showing geographic data."""
+
+import os
+import altair as alt
+import numpy as np
+import pandas as pd
+import pydeck as pdk
+import streamlit as st
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
+
+
+# LOAD DATA ONCE
+# https://docs.streamlit.io/library/api-reference/performance/st.cache_resource
+@st.cache_resource # cache data so it's only loaded once
+def load_data():
+    path = "uber-raw-data-sep14.csv.gz"
+    if not os.path.isfile(path):
+        path = f"https://github.com/streamlit/demo-uber-nyc-pickups/raw/main/{path}"
+
+    data = pd.read_csv(
+        path,
+        nrows=100000,  # approx. 10% of data
+        names=[
+            "date/time",
+            "lat",
+            "lon",
+        ],  # specify names directly since they don't change
+        skiprows=1,  # don't read header since names specified directly
+        usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
+        parse_dates=[
+            "date/time"
+        ],  # set as datetime instead of converting after the fact
+    )
+
+    return data
+
+data = load_data()
+hour = st.slider("Hour to look at", 0, 23) # add sidebar slider to select hour
+data = data[data["date/time"].dt.hour == hour]
+
+## Geo Data
+st.map(data)
+
+f'RAW DATA AT {hour}:00', data
